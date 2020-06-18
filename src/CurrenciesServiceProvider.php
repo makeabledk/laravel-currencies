bf6461fed0a341162e04248ff1c0ebf65136ca49
Rasmus Nielsen <rasmus@easify.dk>
2020-06-18 13:29:43 +0200

    Apply fixes from StyleCI (#19)

--- src/CurrenciesServiceProvider.php
+++ src/CurrenciesServiceProvider.php
@@ -10,7 +10,7 @@ class CurrenciesServiceProvider extends ServiceProvider
     public function boot()
     {
         if (! class_exists('CreateCurrenciesTable')) {
-            $this->publishes([__DIR__.'/../database/migrations/create_currencies_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_currencies_table.php'),]);
+            $this->publishes([__DIR__.'/../database/migrations/create_currencies_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_currencies_table.php')]);
         }
 
         $this->loadTranslationsFrom(__DIR__.'/../resources/lang/', 'laravel-currencies');
