ef799a09a1ac08cde50b5bcd9931f1008c43559b
Rasmus Nielsen <rasmus@easify.dk>
2020-06-22 07:56:07 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Contracts/ResolvesModelCurrency.php
+++ src/Contracts/ResolvesModelCurrency.php
@@ -2,8 +2,6 @@
 
 namespace Makeable\LaravelCurrencies\Contracts;
 
-use Illuminate\Database\Eloquent\Model;
-
 interface ResolvesModelCurrency
 {
     public function resolveModelCurrency(string $field, array $attributes): CurrencyContract;
